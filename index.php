0aa4b1ece3937f1995b0c2b8514a35b4caad94ca
rzainullov <rashid.zainullov@gmail.com>
2022-08-24 14:15:13 +0400

    добавление простой формы поиска

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <title>Test task</title>
 </head>
 <body>
-    
+    <form>
+       <label for="search">Поиск комментария по его фрагменту:<br>
+            <input type="text" id="searched" name="searched" placeholder="Введите фрагмент комментария...">
+       </label> 
+    </form>
 </body>
 </html>
