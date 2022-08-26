50fce1935cd14a4a9e1aa1e7c59132964576574d
rzainullov <rashid.zainullov@gmail.com>
2022-08-26 14:31:26 +0400

    обернул тескт запроса insert в real_escape_string

--- create_db_and_insert_data.php
+++ create_db_and_insert_data.php
@@ -3,6 +3,6 @@
         require "connection.php";
         require_once "init_query.php";
         require_once "insert_data_query.php";
-        return $connection->multi_query(init_query().insert_data_query());
+        return $connection->multi_query($connection->real_escape_string(init_query().insert_data_query()));
     }
 ?>
