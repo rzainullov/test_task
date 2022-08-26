018b03b5b84b6bc114b43157102be4069ddd39b7
rzainullov <rashid.zainullov@gmail.com>
2022-08-26 11:41:15 +0400

    испавлено на require вместо require_once

--- create_db_and_insert_data.php
+++ create_db_and_insert_data.php
@@ -1,6 +1,6 @@
 <?php
     function create_db_and_insert_data() {
-        require_once "connection.php";
+        require "connection.php";
         require_once "init_query.php";
         require_once "insert_data_query.php";
         return $connection->multi_query(init_query().insert_data_query());
