5a1d4c9f83a05f9ec0017246fdf01483b54b3d09
rzainullov <rashid.zainullov@gmail.com>
2022-08-26 11:47:50 +0400

    вставка вызова скрипта в index.php, получающего строку с данныи о количестве записей и комментариев

--- index.php
+++ index.php
@@ -9,9 +9,10 @@
 <body>
     <?php
         require_once "create_db_and_insert_data.php";
-        if(create_db_and_insert_data()) { 
-            echo "<script>console.log('База создана и данные загружены')</script>";
-        }
+        require_once "get_data_count.php";
+        if(create_db_and_insert_data()) {
+            echo "<script>console.log(". get_data_count() . ")</script>";
+        } 
     ?>
     <form id="search_form">
        <label for="search">Поиск комментария по его фрагменту:<br>
